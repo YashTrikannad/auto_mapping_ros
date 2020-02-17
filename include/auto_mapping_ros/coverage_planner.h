bb8335b0ebd1ff92ec9b09de98664bbbcae6c8de
VishnuPrem <vishnu.prem06@gmail.com>
2020-02-16 21:58:59 -0500

    update

--- include/auto_mapping_ros/coverage_planner.h
+++ include/auto_mapping_ros/coverage_planner.h
@@ -2,9 +2,26 @@
 #define AUTO_MAPPING_ROS_COVERAGE_PLANNER_H
 
 #include "graph_builder.h"
+#include <queue>
 
 namespace amr
 {
+    struct MSTNode
+    {
+        explicit MSTNode(const Node &n) : node(&n), key(INT_MAX)
+        {}
+
+        const Node* node;
+        int key;
+        std::vector<MSTNode*> mst_child;
+
+    };
+
+    struct MST_key_greater_than {
+        bool operator()(MSTNode const& a, MSTNode const& b) const {
+            return a.key > b.key;
+        }
+    };
 
 using Sequence = std::vector<std::array<int, 2>>;
 
@@ -15,7 +32,10 @@ public:
     {}
 
     //TODO: Implement Coverage Planning algorithm to solve TSP
-    void compute_sequence();
+    void compute_sequence()
+    {
+        compute_MST();
+    }
 
     Sequence get_sequence()
     {
@@ -26,6 +46,33 @@ public:
 private:
     Graph graph_;
     Sequence sequence_;
+
+    void compute_MST()
+    {
+        std::vector<MSTNode*> open_set;
+        std::vector<MSTNode*> MST;
+
+        for(auto& node: graph_)
+        {
+            MSTNode mstnode(node);
+            open_set.emplace_back(&mstnode);
+        }
+
+        open_set[0]->key = 0;
+        std::make_heap(open_set.begin(), open_set.end(), MST_key_greater_than());
+
+
+
+        while(!open_set.empty())
+        {
+            MSTNode* min_key_node = open_set.front();
+            std::pop_heap (open_set.begin(),open_set.end());
+            open_set.pop_back();
+
+
+        }
+
+    }
 };
 
 }
